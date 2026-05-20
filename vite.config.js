ceadbe2e7c6919449fce61a6a0d7f1fba659d6da
DiegoSR11 <sapaicoramirez11@gmail.com>
2026-05-20 17:46:27 -0500

    Branding: Logo de pestaña, titulo de app y config de Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.js.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Aumentamos el límite de advertencia a 1600 KB
+    chunkSizeWarningLimit: 1600,
+  }
 })
